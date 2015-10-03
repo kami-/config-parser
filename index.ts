6f019f6d916f102a534c15baa2bf7716a45aa898
László Major <laszlo.major@outlook.com>
2015-10-03 12:29:35 +0200

    Moved source files to src folder.

--- index.ts
+++
@@ -1,6 +0,0 @@
-import Lexer = require('./Lexer');
-import Parser = require('./Parser');
-import PrettyPrinter = require('./PrettyPrinter');
-import Ast = require('./Ast');
-import Mission = require('./Mission');
-
