f47411138c83f677450be2404d94961d852f1250
László Major <laszlo.major@outlook.com>
2015-12-20 20:45:22 +0100

    Updated to new Typescript and configured for npm compatibility.

---
+++ src/ts/index.ts
@@ -0,0 +1,11 @@
+export import Ast = require('./Ast');
+export import Lexer = require('./Lexer');
+export import Mission = require('./Mission');
+export import Parser = require('./Parser');
+export import PrettyPrinter = require('./PrettyPrinter');
+/*import * as Lexer from './Lexer';
+import * as Mission from './Mission';
+import * as Parser from './Parser';
+import * as PrettyPrinter from './PrettyPrinter';
+
+export var Ast = Ast;*/
