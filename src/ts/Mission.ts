7b82e5a2c67375470fcd93d943b2b83432b37456
László Major <laszlo.major@outlook.com>
2016-04-09 14:44:22 +0200

    Support for ids in new mission sqm format.

--- src/ts/Mission.ts
+++ src/ts/Mission.ts
@@ -16,9 +16,27 @@ function updateItemClassNames(allItems: Parser.Node[]): void {
     }
 }
 
-export function mergeItems(node: Parser.Node, newItems: Parser.Node[]): void {
+function updateIds(node: Parser.Node): number {
+    var items = Ast.select(node, 'Item*');
+    var count = 0;
+    for (var i = 0, len = items.length; i < len; i++) {
+        if (Ast.select(items[i], 'dataType')[0].value = 'Group') {
+            var groupItems = Ast.select(items[i], 'Entities.Item*');
+            for (var j = 0, groupLen = groupItems.length; j < groupLen; j++) {
+                Ast.select(groupItems[j], 'id')[0].value = count + 1;
+                count++;
+            }
+        }
+        Ast.select(items[i], 'id')[0].value = count + 1;
+        count++;
+    }
+    return count;
+}
+
+export function mergeItems(node: Parser.Node, newItems: Parser.Node[]): number {
     node.fields = node.fields.concat(newItems);
     var allItems: Parser.Node[] = Ast.select(node, 'Item*');
     updateItemsField(node, allItems.length);
     updateItemClassNames(allItems);
+    return updateIds(node);
 }
